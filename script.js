e94b6c2debaed96607e908bcfb4c574a1d785dad
Johané Badenhorst <577401@student.belgiumcampus.ac.za>
2024-02-14 10:23:04 +0200

    Javascript removed

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
 // Preload Images
 var images = new Array();
+var path = /images/;
 
-function preloadImages()
+function preloadImages(path) 
 {
     for (i=0; i < preloadImages.arguments.length; i++)
     {
         images[i] = new Image();
 
-        images[i].src = preloadImages.arguments[i];
+        images[i].src = path + preloadImages.arguments[i];
     }
 }
 
-preloadImages("RS_Logo.png","RS-Logo_Background.png","consulting.jpg","HSE.jpg","lifting.jpg","mechanical.jpg","rigging.jpg","sourcing.jpg","structural.jpg","technical.jpg","transporting.jpg");
-
-// No Right Click
-/*function f1() 
-{
-    if(document.all) { return false; }
-
-}
-
-function f2(e) 
-{
-    if(document.layers || (document.getElementById &! document.all)) 
-    {
-      if(e.which==2 || e.which==3) { return false; }
-    }
-}
-  
-if(document.layers) 
-{
-    document.captureEvents(Event.MOUSEDOWN);
-    document.onmousedown = f1;
-}
-else 
-{
-    document.onmouseup = f2;
-    document.oncontextmenu = f1;
-}
-document.oncontextmenu = new function("return false");*/
+preloadImages("images/","RS_Logo.png","RS-Logo_Background.png","consulting.jpg","HSE.jpg","lifting.jpg","mechanical.jpg","rigging.jpg","sourcing.jpg","structural.jpg","technical.jpg","transporting.jpg");
 
 //Show Current Date
 function showDate() 
@@ -51,16 +25,16 @@ function showDate()
 }
 
 // Email Validation
-function validateEmail(theForm) 
+/*function validateEmail(form) 
 {
-    if (/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/.test(theForm.email-id.value))
+    if (/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/.test(form.email.value))
     {
         return(true);
     }
 
     alert("Invalid e-mail address! Please enter again carefully!.");
     return(false);
-}
+}*/
 
 /*Body:
 
@@ -79,5 +53,4 @@ function scrollToTop()
         behavior: 'smooth'
     });
 }
-
 
